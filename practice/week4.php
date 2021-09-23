9b22a887aeebabcf5c241bd6a2ec1928e27daae9
Michael Jacobs <mike32jacobs@gmail.com>
2021-09-23 09:43:49 +0000

    Started week4notes

--- practice/week4.php
+++ practice/week4.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "Hello from week4.php";
+$moves = ['rock', 'paper','scissors'];
+
+#           1,1,2,3,4,5
+$strawLengths = [2, 2, 2, 2, 2, 1];
+
+$mixed = ['rock', 1, .04, true];
+
+$randomNumber = rand(0, 2);
+
+$move = $moves[$randomNumber];
+
+var_dump($moves);
+var_dump($move);
