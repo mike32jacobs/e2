8d8c87899b8bc0a62926c3d734cfbc80f2e134cb
Michael Jacobs <mike32jacobs@gmail.com>
2021-10-22 19:53:32 +0000

    Started P2

---
+++ p2/index-view.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+    <title>Project 2: BlackJack</title>
+    <meta charset='utf-8'>
+    <link href=data: , rel=icon>
+</head>
+
+<body>
+
+    <h1>Project 2 BlackJack</h1>
+
+</body>
+
+</html>
