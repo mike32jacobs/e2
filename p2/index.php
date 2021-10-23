2f70236ea8680895c20a839bd9aad5966be71fee
Michael Jacobs <mike32jacobs@gmail.com>
2021-10-23 13:31:43 +0000

    Started game logic. Just a random save.

--- p2/index.php
+++ p2/index.php
@@ -1,5 +1,37 @@
 <?php
 
+
+$deck = build_deck();
+shuffle($deck);
+
+// Create an array players in the game. This is hard coded, but it will eventually make it easier to let the user choose the number of players.
+
+$numberOfPlayers = 4;
+
+$players = array();
+
+for ($i = 0; $i<$numberOfPlayers; $i++)
+{
+    // Convert i to a string
+    $pNumber = strval($i+1);
+    array_push($players,'p'.$pNumber);
+
+}
+var_dump($playerNames);
+
+// Deal to each player in the game
+
+foreach ($players as $player){
+    $topCard = array_shift($deck);
+    array_push($player1Hand, $topCard);
+
+    // The player gets two cards off of the deck
+    
+}
+
+// Deal to the dealer
+
+
 // user definted function to build a deck
 
 function build_deck()
@@ -21,6 +53,12 @@ function build_deck()
 
     return $deck;
 }
-$deck = build_deck();
+
+// User defined funtion to deal to a player. This may not be needed in a one player game, but it will be needed if there are multiple players, or if a player splits and has two hands.
+
+function deal_to_player($player){
+
+
+}
 
 require 'index-view.php';
