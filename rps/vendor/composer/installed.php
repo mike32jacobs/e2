a67ce88e0ddd809308a55cd196f6d11c92a4a766
Michael Jacobs <mike32jacobs@gmail.com>
2021-10-30 10:49:39 +0000

    Installed Rock Paper Scissors.

---
+++ rps/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '51ebe6a70ba5f1300e05672e1c49629efca2d83a',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '51ebe6a70ba5f1300e05672e1c49629efca2d83a',
+            'dev_requirement' => false,
+        ),
+        'susanbuck/rock-paper-scissors' => array(
+            'pretty_version' => '0.1.5',
+            'version' => '0.1.5.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../susanbuck/rock-paper-scissors',
+            'aliases' => array(),
+            'reference' => '2b8a34339715507b1b9f7664478ff51bdc9ff219',
+            'dev_requirement' => false,
+        ),
+    ),
+);
