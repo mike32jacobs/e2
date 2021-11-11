59f6b123878cf5a3369cdb46627a828a6b83688f
Michael Jacobs <mike32jacobs@gmail.com>
2021-11-11 16:03:36 +0000

    After Week 11 Part 2

--- zipfoods/views/about.blade.php
+++ zipfoods/views/about.blade.php
@@ -1,3 +1,17 @@
-<h1> About</h1>
+@extends('templates/master')
+
+@section('title')
+About
+@endsection
+
+@section('head')
+<link href='/css/about.css' rel='stylesheet'>
+@endsection
+
+@section('content')
+
+<h2>About</h2>
 
 {{ $aboutMessage }}.
+
+@endsection
--- zipfoods/views/contact.blade.php
+++ zipfoods/views/contact.blade.php
@@ -1,3 +1,11 @@
-<h1> Support and Contact info</h1>
+@extends('templates/master')
+
+@section('title')
+Support and Contact info
+@endsection
+
+@section('content')
+<h2> Support and Contact info</h2>
 
 Have questions? Email us at {{ $email }}.
+@endsection
